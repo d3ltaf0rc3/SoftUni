50ec26bc73b92766f05bf128c06d083c5ee170af
Martin Marinov <>
2020-02-13 18:23:05 +0200

    Added some missing exercises

---
+++ 03.JS-Advanced/02.Arrays/Exercises/sort-arr.js
@@ -0,0 +1,16 @@
+function sortBy2(strings) {
+    strings.sort((a, b) => {
+        let result = a.length - b.length;
+        if (result === 0) {
+            return a.localeCompare(b);
+        } else {
+            return result;
+        }
+    });
+
+    strings.forEach(element => {
+        console.log(element);
+    });
+}
+
+sortBy2();
