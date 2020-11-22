f5693bf13d3921ec0b21a3ede8ad969bdffdec78
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-11-22 20:51:39 +0200

    feat: login and guest home page

--- 08.Angular/softuni-forum-workshop/softuni-forum/src/app/home/home.component.ts
+++ 08.Angular/softuni-forum-workshop/softuni-forum/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements DoCheck {
+  isLogged = false;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.isLogged = this.userService.isLogged;
   }
-
 }
